f173b15ec1ee745edf9f5ca7bebf1cdebf815843
Christopher Bernhard <c.bernhard@mps-solutions.de>
2018-06-06 15:56:41 +0200

    Added Output of Number into JS

--- src/android/PhoneCallReceiver.java
+++ src/android/PhoneCallReceiver.java
@@ -7,6 +7,9 @@ import java.lang.StringBuilder;
 import java.lang.String;
 import android.util.Log;
 import android.widget.Toast;
+import android.telephony.TelephonyManager;
+import android.content.pm.PackageManager;
+
 
 public class PhoneCallReceiver extends BroadcastReceiver {
 
@@ -14,12 +17,22 @@ public class PhoneCallReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+		// build log
         StringBuilder sb = new StringBuilder();
         sb.append("Action: " + intent.getAction() + "\n");
         sb.append("URI: " + intent.toUri(Intent.URI_INTENT_SCHEME).toString() + "\n");
         String log = sb.toString();
         Log.d(TAG, log);
         Toast.makeText(context, log, Toast.LENGTH_LONG).show();
+		
+		//read incoming number
+		String incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+		
+		// start app with params
+		PackageManager pm = context.getPackageManager();
+		Intent launchIntent = pm.getLaunchIntentForPackage("de.mpssolutions.twin.mobile");
+		launchIntent.putExtra("incoming_number", incomingNumber);
+		context.startActivity(launchIntent);
     }
 
 }
